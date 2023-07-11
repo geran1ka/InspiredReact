661c379834a811642d8ee3f11580d10dc3c0c485
geran1ka <roman.khoruzhy@gmail.com>
2023-07-11 06:56:36 +0300

    day 5 Домашнее заданее

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -9,13 +9,20 @@ import { Banner } from "../Banner/Banner.jsx";
 export const MainPage = () => {
   const {gender, category} = useParams();
   const dispatch = useDispatch();
-  const {activeGender, categories} = useSelector(state => state.navigation);
+  const {activeGender, categories, genderList} = useSelector(state => state.navigation);
   const genderData = categories[activeGender];
+
+  const categoryData = genderData?.list.find(item => item.slug === category)
 
 
 useEffect(() => {
-  dispatch(setActiveGender(gender));
-}, [gender, dispatch])
+  if (gender) {
+    dispatch(setActiveGender(gender));
+  } else if (genderList[0]) {
+    dispatch(setActiveGender(genderList[0]));
+    dispatch(fetchGender(genderList[0]));
+  }
+}, [gender, genderList, dispatch])
 
   useEffect(() => {
     if (gender && category) {
@@ -30,8 +37,7 @@ useEffect(() => {
 
   return (
     <>
-      <Banner data={genderData?.banner}/>
-      <Goods categoryData={genderData?.list
-    .find(item => item.slug === category)} />
+      {!category && <Banner data={genderData?.banner} />}
+      <Goods categoryData={categoryData} />
     </>
   )};
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -4,10 +4,14 @@ import { ReactComponent as Like } from '../../assets/heart.svg';
 import s from './Product.module.scss';
 import { ColorList } from '../ColorList/ColorList';
 
-export const Product = ({id, pic, title, price, colors}) => (
+export const Product = ({id, pic, title, price, colors, description}) => (
   <article className={s.product}>
-    <NavLink to={`/product/${id}`} className={s.link}>
-      <img className={s.image} src={`${API_URL}${pic}`}/>
+    <NavLink to={`/product/${id}`} className={s.link} >
+      <img 
+        className={s.image} 
+        src={`${API_URL}${pic}`}
+        alt={`${title} ${description}`}
+      />
       <h3 className={s.title}>{title}</h3>
     </NavLink>
 
