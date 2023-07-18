72d98aa9552b379f2a736b72e9c186f847a7b856
geran1ka <roman.khoruzhy@gmail.com>
2023-07-18 06:46:20 +0300

    day 6.2 21minuta

--- src/components/ProductPage/ProductPage.jsx
+++ src/components/ProductPage/ProductPage.jsx
@@ -1,8 +1,28 @@
+import { useEffect } from 'react';
+import { Container } from '../Layout/Container/Container';
 import s from './ProductPage.module.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProduct } from '../../features/productSlice';
+import { useParams } from 'react-router-dom';
+import { API_URL } from '../../const';
 
 export const ProductPage = () => {
 
+  const dispatch = useDispatch();
+  const {id} = useParams();
+
+  const { product } = useSelector(state => state.product);
+
+  useEffect(() => {
+    dispatch(fetchProduct(id))
+  }, [id, dispatch]);
+
+
   return (
-    <div>ProductPage</div>
+    <section className={s.card}>
+      <Container className={s.container}>
+        <img src={`${API_URL}${product.pic}`} alt={`${product.title} ${product.description}`}></img>
+      </Container>
+    </section>
   )
 }
