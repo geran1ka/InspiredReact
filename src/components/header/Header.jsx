38672e2a3c9406b83c10c02462e5f4cb67cc25a5
Geran1ka <roman.khoruzhy@gmail.com>
2023-07-20 15:06:41 +0300

    order & search

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,13 @@
 import {Top} from './Top/Top.jsx';
 import {Navigation} from './Navigation/Navigation.jsx';
 import s from './Header.module.scss';
+import {Search} from '../Search/Search.jsx';
+import {useState} from 'react';
 
 export const Header = () => (
   <header className={s.header}>
     <Top />
+    <Search />
     <Navigation />
   </header>
-)
+);
--- src/components/header/Top/Top.jsx
+++ src/components/header/Top/Top.jsx
@@ -1,30 +1,36 @@
-import { Container } from '../../Layout/Container/Container.jsx';
+import {Container} from '../../Layout/Container/Container.jsx';
 import s from './Top.module.scss';
 import cn from 'classnames';
 import logo from '/src/assets/logo.svg';
-import { NavLink } from 'react-router-dom';
-import { ReactComponent as LikeSVG } from '../../../assets/heart.svg';
-import { ReactComponent as SearchSVG } from '../../../assets/search.svg';
-import { ReactComponent as CartSVG } from '../../../assets/cart.svg';
-import { useSelector } from 'react-redux';
-
+import {NavLink} from 'react-router-dom';
+import {ReactComponent as LikeSVG} from '../../../assets/heart.svg';
+import {ReactComponent as SearchSVG} from '../../../assets/search.svg';
+import {ReactComponent as CartSVG} from '../../../assets/cart.svg';
+import {useDispatch, useSelector} from 'react-redux';
+import { toggleSearch } from '../../../features/searchSlice.js';
 
 
 export const Top = () => {
   const {countItems} = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+
+  const handleOpenSearch = () => {
+    dispatch(toggleSearch());
+  };
+
   return (
     <div className={s.top}>
       <Container className={s.container}>
         <a className={cn(s.link, s.phone)} href='tel:89304902620'>8 930 490 26 20</a>
-      
+
         <NavLink className={s.logo} to='/'>
           <img src={logo} alt='Логотип Inspired'></img>
         </NavLink>
-  
+
         <div className={s.navigation}>
           <ul className={s.navList}>
             <li className={s.navItem}>
-              <button className={s.link}>
+              <button className={s.link} onClick={handleOpenSearch}>
                 <SearchSVG />
               </button>
             </li>
@@ -43,5 +49,5 @@ export const Top = () => {
         </div>
       </Container>
     </div>
-  )
-}
+  );
+};
