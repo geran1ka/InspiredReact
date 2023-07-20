ea15a6daeeade05b51a82a263c18c38e7690406b
geran1ka <roman.khoruzhy@gmail.com>
2023-07-20 22:41:47 +0300

    status Server

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,43 @@
-import { useRouteError } from "react-router-dom"
-
+import { useLocation, useNavigate, useRouteError } from "react-router-dom"
+import s from './ErrorPage.module.scss'
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useRef } from "react";
+import { fetchColors } from "../../features/colorSlice.js";
+import { fetchNavigation } from "../../features/navigationSlice.js";
 export const ErrorPage = () => {
-  const error = useRouteError();
+  const routeError = useRouteError();
+  const {status} = useSelector(state => state.statusServer);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const timeIdRef = useRef(null);
+
+
+  useEffect(() => {
+    if (status && location.pathname === '/404') {
+      navigate('/')
+    }
+  }, [navigate, status, location]);
+
+  
+  useEffect(() => {
+    if (!status && location.pathname === '/404') {
+      clearInterval(timeIdRef.current);
+
+      timeIdRef.current = setInterval(() => {
+        dispatch(fetchNavigation());
+        dispatch(fetchColors());
+      }, 3000);
+    }
+    return () => {
+      clearInterval(timeIdRef.current);
+    }
+  }, [dispatch, status, location])
 
   return (
-  <div>
-    <h2>Ошибка 404</h2>
-    <p>{error?.message ?? 'Неизвестная ошибка'}</p>
+  <div className={s.error}>
+    <h2 className={s.title}>Произошла ошибка, попробуйте зайти попозже!</h2>
+    <p className={s.message}> {routeError?.message ?? 'Неизвестная ошибка'}</p>
   </div>
 )};
--- src/components/Layout/Main/Main.jsx
+++ src/components/Layout/Main/Main.jsx
@@ -1,4 +1,19 @@
 //import cn from 'classnames';
+import { useSelector } from 'react-redux';
 import s from './Main.module.scss'
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-export const Main = ({children}) => <div className={s.main}>{children}</div>;
+export const Main = ({children}) => {
+  const {status} = useSelector(state => state.statusServer);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!status && location.pathname !== '/404') {
+      navigate('/404')
+    }
+  })
+  return <div className={s.main}>{children}</div>
+};
